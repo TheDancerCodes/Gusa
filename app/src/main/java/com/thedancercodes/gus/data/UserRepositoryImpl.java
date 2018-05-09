59db63ca2cd5fb31566eba9c34e89f0a8a12c84f
Roger Taracha <rojtaracha@gmail.com>
2018-05-09 16:56:06 +0300

    [FEATURE] Create Web Service Calls PT3:
    [+] This is where we combine the two network calls and
    transform the data into a view that will be used in the front end.
    
    [+] We are using lambda expressions in the code by building our app with the new Jack toolchain.
    
      [=] Read about how to enable building for Java 8 on Android:
      [=] https://developer.android.com/studio/write/java8-support

--- app/src/main/java/com/thedancercodes/gus/data/UserRepositoryImpl.java
+++ app/src/main/java/com/thedancercodes/gus/data/UserRepositoryImpl.java
@@ -1,8 +1,43 @@
 package com.thedancercodes.gus.data;
 
-/**
- * Created by TheDancerCodes on 09/05/2018.
- */
+import com.thedancercodes.gus.data.remote.GithubUserRestService;
+import com.thedancercodes.gus.data.remote.model.User;
 
-public class UserRepositoryImpl {
+import java.io.IOException;
+import java.util.List;
+
+import rx.Observable;
+
+
+/**
+ * This is where we will combine the two network calls and transform the data into a view that
+ * will be used in the front end.
+ *
+ * This is using RxJava to first get a list of users for the search term and
+ * then for each user it does another network call to find out more of the user’s information.
+ */
+public class UserRepositoryImpl implements UserRepository {
+
+    private GithubUserRestService mGithubUserRestService;
+
+    public UserRepositoryImpl(GithubUserRestService githubUserRestService) {
+        this.mGithubUserRestService = githubUserRestService;
+    }
+
+
+    // TODO: You might want to add a terminating condition; Only retrying a certain no. of times
+    @Override
+    public Observable<List<User>> searchUsers(final String searchTerm) {
+        return Observable.defer(() -> mGithubUserRestService.searchGithubUsers(searchTerm)
+                .concatMap(usersList -> Observable.from(usersList.getItems())
+                .concatMap(user -> mGithubUserRestService.getUser(user.getLogin())).toList()))
+                .retryWhen(observable -> observable.flatMap(o -> {
+                    if (o instanceof IOException) {
+                        return Observable.just(null);
+                    }
+                    return Observable.error(o);
+                }));
+
+    }
+
 }
