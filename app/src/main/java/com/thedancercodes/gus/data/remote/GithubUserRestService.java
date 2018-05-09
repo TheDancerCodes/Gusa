4be53254cac2f169f093b504731826d93c642dd6
Roger Taracha <rojtaracha@gmail.com>
2018-05-09 16:12:18 +0300

    [FEATURE] Create Web Service Calls PT2:
    [+] Create the Retrofit calls.

--- app/src/main/java/com/thedancercodes/gus/data/remote/GithubUserRestService.java
+++ app/src/main/java/com/thedancercodes/gus/data/remote/GithubUserRestService.java
@@ -1,8 +1,20 @@
 package com.thedancercodes.gus.data.remote;
 
-/**
- * Created by TheDancerCodes on 09/05/2018.
- */
+import com.thedancercodes.gus.data.remote.model.User;
+import com.thedancercodes.gus.data.remote.model.UsersList;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import rx.Observable;
 
 public interface GithubUserRestService {
+
+    // This network call performs a search to get a list of users
+    @GET("/search/users?per_page=2")
+    Observable<UsersList> searchGithubUsers(@Query("q") String searchTerm);
+
+    // This network call gets more details about a user.
+    @GET
+    Observable<User> getUser(@Path("username") String username);
 }
