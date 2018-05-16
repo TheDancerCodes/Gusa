c096049e1ea31a2521013e4c86de83f48e795cfc
Roger Taracha <rojtaracha@gmail.com>
2018-05-16 17:07:38 +0300

    [FEATURE] Creating the Presenter PT2:
    [+] BasePresenter provides functionality to check whether:
       [=] A View is attached to the Presenter &
       [=] A way to manage RxJava Subscriptions.

--- app/src/main/java/com/thedancercodes/gus/presentation/base/BasePresenter.java
+++ app/src/main/java/com/thedancercodes/gus/presentation/base/BasePresenter.java
@@ -1,8 +1,58 @@
 package com.thedancercodes.gus.presentation.base;
 
-/**
- * Created by TheDancerCodes on 09/05/2018.
- */
 
-public class BasePresenter {
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * BasePresenter provides functionality to check whether a view is attached to the presenter
+ * and a way to manage RxJava subscriptions.
+ *
+ * @param <T>
+ */
+public class BasePresenter<T extends MvpView> implements MvpPresenter<T> {
+
+    private T view;
+
+    // This object holds a group of RxJava subscriptions.
+    private CompositeSubscription compositeSubscription = new CompositeSubscription();
+
+    @Override
+    public void attachView(T mvpView) {
+        view = mvpView;
+    }
+
+    @Override
+    public void detachView() {
+        // unsubscribe from all subscriptions; prevent memory leaks and view crashes
+        compositeSubscription.clear();
+        view = null;
+    }
+
+    public T getView() {
+        return view;
+    }
+
+    public void checkViewAttached() {
+        if (!isViewAttached()) {
+            throw new MvpViewNotAttachedException();
+        }
+    }
+
+    private boolean isViewAttached() {
+        return view != null;
+    }
+
+    //  When a subscription is created in a presenter that subclasses this object
+    // (CompositeSubscription), we will call addSubscription()
+    protected void addSubscription(Subscription subscription) {
+        this.compositeSubscription.add(subscription);
+    }
+
+    protected static class MvpViewNotAttachedException extends RuntimeException {
+        public MvpViewNotAttachedException() {
+            super("Please call Presenter.attachView(MvpView) before"
+                    + "requesting data to the Presenter");
+        }
+    }
 }
