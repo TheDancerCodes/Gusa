d2d4bd538d5e6b53f46fd16d1300bd5a83dcf6cc
Roger Taracha <rojtaracha@gmail.com>
2018-05-16 23:38:30 +0300

    [FEATURE] Creating the Presenter PT3:
    [+] Create Contracts between the View and the Presenter.

--- app/src/main/java/com/thedancercodes/gus/presentation/search/UserSearchContract.java
+++ app/src/main/java/com/thedancercodes/gus/presentation/search/UserSearchContract.java
@@ -1,8 +1,27 @@
 package com.thedancercodes.gus.presentation.search;
 
-/**
- * Created by TheDancerCodes on 09/05/2018.
- */
+import com.thedancercodes.gus.data.remote.model.User;
+import com.thedancercodes.gus.presentation.base.MvpPresenter;
+import com.thedancercodes.gus.presentation.base.MvpView;
 
-public interface UserSearchContract {
+import java.util.List;
+
+/**
+ * Contracts between the View and the Presenter.
+ */
+interface UserSearchContract {
+
+    interface View extends MvpView {
+        void showSearchResults(List<User> githubUserList);
+
+        void showError(String message);
+
+        void showLoading();
+
+        void hideLoading();
+    }
+
+    interface Presenter extends MvpPresenter<View> {
+        void search(String term);
+    }
 }
